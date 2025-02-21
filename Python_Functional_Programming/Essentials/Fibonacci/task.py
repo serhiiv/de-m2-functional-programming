5ad2e1dbb0bbbdf64453b08c0852664aa29f9326
Serhii Vykaliuk <serhii.v@gmail.com>
2025-02-21 17:37:40 +0200

    Add Fibonacci search implementation and tests

---
+++ Python_Functional_Programming/Essentials/Fibonacci/task.py
@@ -0,0 +1,23 @@
+from typing import Callable
+
+
+# Implement Fibonacci search using the simples functional recursion
+def fibonacci_impl() -> Callable[[int], int]:
+    def fib(n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)
+
+    return fib
+
+
+if __name__ == "__main__":
+    print(fibonacci_impl()(0))
+    print(fibonacci_impl()(1))
+    print(fibonacci_impl()(2))
+    print(fibonacci_impl()(3))
+    print(fibonacci_impl()(4))
+    print(fibonacci_impl()(25))
